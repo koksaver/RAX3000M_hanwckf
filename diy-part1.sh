2f4666a3a48014823c2491961792819016823afe
aliass532 <56620619+aliass532@users.noreply.github.com>
2025-01-04 16:58:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
