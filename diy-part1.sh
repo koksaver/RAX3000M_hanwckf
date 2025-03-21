1765668ddc9bac9b89c851505df35309b53d99a2
aliass532 <56620619+aliass532@users.noreply.github.com>
2025-03-21 20:14:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 #echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git 5G_Modem_Support https://github.com/Siriling/5G-Modem-Support;main' >> feeds.conf.default
 echo >> feeds.conf.default
